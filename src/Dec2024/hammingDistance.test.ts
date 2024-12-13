38a88041e3330b60149680732789a2ff85a241ff
Mindi Weik <mindi@sierrainteractive.com>
2024-12-13 16:43:53 -0700

    hammingDistance

--- src/Dec2024/hammingDistance.test.ts
+++ src/Dec2024/hammingDistance.test.ts
@@ -2,8 +2,38 @@ import assert from 'assert'
 import { hammingDistance } from './hammingDistance'
 
 describe(hammingDistance.name, () => {
-  it('should describe the expected result', () => {
-    const actual = 'actual'
-    assert.deepStrictEqual(actual, 'actual')
-  })
+  it('should return 0', () => {
+    const actual = hammingDistance('', '')
+    assert.deepStrictEqual(actual, 0, `expected 0, received ${actual}`)
+	})
+	
+	it('should return 3', () => {
+		const actual = hammingDistance('I like turtles', 'I like turkeys')
+		assert.deepStrictEqual(actual, 3, `expected 3, received ${actual}`)
+	})
+
+	it('should return 0', () => {
+		const actual = hammingDistance('Hello World', 'Hello World')
+		assert.deepStrictEqual(actual, 0, `expected 0, received ${actual}`)
+	})
+
+	it('should return 3', () => {
+		const actual = hammingDistance('a man a plan a canal', 'a man a plan sobanal')
+		assert.deepStrictEqual(actual, 3, `expected 3, received ${actual}`)
+	})
+
+	it('should return 2', () => {
+		const actual = hammingDistance('hamming and cheese', 'Hamming and Cheese')
+		assert.deepStrictEqual(actual, 2, `expected 2, received ${actual}`)
+	})
+
+	it('should return 2', () => {
+		const actual = hammingDistance('espresso', 'Expresso')
+		assert.deepStrictEqual(actual, 2, `expected 2, received ${actual}`)
+	})
+
+	it('should return 24', () => {
+		const actual = hammingDistance('old father, old artificer', 'of my soul the uncreated ')
+		assert.deepStrictEqual(actual, 24, `expected 24, received ${actual}`)
+	})
 })
