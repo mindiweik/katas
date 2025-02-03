9978ec76980b0c887669a1617374143ac3d4eebb
Mindi Weik <57885108+mindiweik@users.noreply.github.com>
2025-02-03 12:43:05 -0700

    Jan 2025 - Week of Jan 27 (#5)
    * set up files for this week's katas
    
    * update packages to use tsx
    
    * decipherThis
    
    * hammingDistance
    
    * weekOfJan6 setup
    
    * temp and score katas
    
    * score completed
    
    * rgb completed
    
    * rot13
    
    * fix_countdown

---
+++ src/2025/01Jan/rot13.test.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import { rot13 } from './rot13'
+
+describe(rot13.name, () => {
+  it('should return "ROT13 example."', () => {
+    const actual = rot13('EBG13 rknzcyr.')
+    assert.deepStrictEqual(actual, 'ROT13 example.')
+	})
+	
+	it('should return "This is my first ROT13 exercise!"', () => {
+		const actual = rot13('Guvf vf zl svefg EBG13 rkrepvfr!')
+		assert.deepStrictEqual(actual, 'This is my first ROT13 exercise!')
+	})
+})
---
+++ src/2025/01Jan/rot13.ts
@@ -0,0 +1,40 @@
+/**
+ * This solution works, but is a naive solution:
+ */
+
+// export const rot13 = (str: string) => {
+// 	let result = ''
+
+// 	for (let i = 0; i < str.length; i++) {
+// 		const code = str.charCodeAt(i)
+
+// 		if (code >= 65 && code <= 122) {
+// 			if (code + 13 > 122) {
+//         result += String.fromCharCode(code - 13)	
+// 			} else {
+// 				result += String.fromCharCode(code + 13)
+// 			}
+// 		} else {
+// 			result += str[i]
+// 		}
+
+// 	}
+
+// 	return result
+// }
+
+/**
+ * Refactored:
+ */
+
+export const rot13 = (str: string) => {
+	return str.replace(/[A-Za-z]/g, (c) => {
+		return String.fromCharCode(c.charCodeAt(0) + (c.toLowerCase() <= 'm' ? 13 : -13))
+	})
+}
+
+/**
+ * Notes:
+ * 
+ * I also saw this regex: `/[a-z]/gi` which I prefer because it's simpler.
+ */
