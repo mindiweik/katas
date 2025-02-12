6bf9497e8df2c71b6cfde80b2252f67d71c1f6f1
Mindi Weik <mindi@sierrainteractive.com>
2025-02-12 12:04:27 -0700

    scoobydoo

---
+++ src/2025/02Feb/phone.test.ts
@@ -0,0 +1,8 @@
+import assert from 'assert'
+
+describe('func.name', () => {
+  it('should describe the expected result', () => {
+    const actual = 'actual'
+    assert.deepStrictEqual(actual, 'actual')
+  })
+})
---
+++ src/2025/02Feb/scoobydoo.test.ts
@@ -0,0 +1,19 @@
+import assert from 'assert'
+import { scoobydoo, villains } from './scoobydoo'
+
+describe(scoobydoo.name, () => {
+  it('should locate "Medicines Man"', () => {
+    const actual = scoobydoo('ndcddzmiahsz', villains)
+    assert.deepStrictEqual(actual, 'Medicines Man')
+	})
+	
+	it('should locate "Skeletons Men"', () => {
+		const actual = scoobydoo('ooegefsiehsi', villains)
+		assert.deepStrictEqual(actual, 'Skeletons Men')
+	})
+
+	it('should locate "Witch Doctors"', () => {
+		const actual = scoobydoo('oyhxtdwnrjtx', villains)
+		assert.deepStrictEqual(actual, 'Witch Doctors')
+	})
+})
