201ddbdb87b27791c3a112e9899c91ad39e3f8fb
Mindi Weik <mindi@sierrainteractive.com>
2025-02-24 13:34:37 -0700

    multiplesOf3Or5

--- src/2025/02Feb/multiplesOf3Or5.test.ts
+++ src/2025/02Feb/multiplesOf3Or5.test.ts
@@ -2,8 +2,18 @@ import assert from 'assert'
 import { multiplesOf3Or5 } from './multiplesOf3Or5'
 
 describe(multiplesOf3Or5.name, () => {
-  it('should describe the expected result', () => {
-    const actual = 'actual'
-    assert.deepStrictEqual(actual, 'actual')
-  })
+  it('should return 23', () => {
+    const actual = multiplesOf3Or5(10)
+		assert.deepStrictEqual(actual, 23)
+	})
+	
+	it('should return 143', () => {
+		const actual = multiplesOf3Or5(25)
+		assert.deepStrictEqual(actual, 143)
+	})
+
+	it('should return 0 for a negative input', () => {
+		const actual = multiplesOf3Or5(-5)
+		assert.deepStrictEqual(actual, 0)
+	})
 })
