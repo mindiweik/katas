af1ea5bf6d8699ce37af7e5867d3d1ee909a5a6b
Mindi Weik <mindi@sierrainteractive.com>
2025-04-30 12:07:06 -0600

    multiplicationTable

---
+++ src/2025/04Apr/multiplicationTable.test.ts
@@ -0,0 +1,19 @@
+import assert from 'assert'
+import { multiplicationTable } from './multiplicationTable'
+
+describe(multiplicationTable.name, () => {
+  it('should return [[1]]', () => {
+    const actual = multiplicationTable(1)
+    assert.deepStrictEqual(actual, [[1]])
+	})
+	
+	it('should return [[1, 2], [2, 4]]', () => {
+		const actual = multiplicationTable(2)
+		assert.deepStrictEqual(actual, [[1, 2], [2, 4]])
+	})
+
+	it('should return [[1, 2, 3], [2, 4, 6], [3, 6, 9]]', () => {
+		const actual = multiplicationTable(3)
+		assert.deepStrictEqual(actual, [[1, 2, 3], [2, 4, 6], [3, 6, 9]])
+	})
+})
