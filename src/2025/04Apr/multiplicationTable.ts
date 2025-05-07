8becc58f9be62f9cd22eb96808c95dc057c22559
Mindi Weik <57885108+mindiweik@users.noreply.github.com>
2025-05-07 10:53:09 -0600

    Apr 2025 - Week of Apr 28 (#13)
    * balance
    
    * multiplicationTable
    
    * weekOfMay5
    
    * adjust to remove errors

---
+++ src/2025/04Apr/multiplicationTable.ts
@@ -0,0 +1,30 @@
+/**
+ * This solution works, but is a naive solution:
+ */
+
+export function multiplicationTable(size: number): number[][] {
+  const result = [[1]]
+
+  for (let i = 1; i < size; i++) {
+    result[0].push(i + 1)
+    result.push([i + 1])
+  }
+
+  const firstRow = result[0]
+
+  for (let row = 1; row < size; row++) {
+    for (let col = 1; col < size; col++) {
+      result[row][col] = firstRow[col] * result[row][0]
+    }
+  }
+
+  return result
+}
+
+/**
+ * Refactored:
+ */
+
+/**
+ * Notes:
+ */
